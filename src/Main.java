fc2a97d32f790a00e653b87864e72e5f45d9bea0
Michał Terczyński <mterczynski271@gmail.com>
2017-09-06 22:54:54 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,472 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Main {
+
+	private static void zadanie58() throws Exception {
+		
+		Scanner plik_dane1 = new Scanner(new File("Files/58/dane_systemy1.txt"));
+		Scanner plik_dane2 = new Scanner(new File("Files/58/dane_systemy2.txt"));
+		Scanner plik_dane3 = new Scanner(new File("Files/58/dane_systemy3.txt"));
+		
+		PrintWriter printWriter = new PrintWriter(new File("rozwiazania/58.txt"));
+		
+		class Pomiar{
+			public int temperatura;
+			public int czas;
+		}
+		
+		ArrayList<Pomiar> pomiary1 = new ArrayList<Pomiar>();
+		ArrayList<Pomiar> pomiary2 = new ArrayList<Pomiar>();
+		ArrayList<Pomiar> pomiary3 = new ArrayList<Pomiar>();
+		
+		while(plik_dane1.hasNext()) {
+			String czas = plik_dane1.next();
+			String temperatura = plik_dane1.next();
+			
+			Integer czasInt = 0;
+			Integer temperaturaInt = 0;
+			// zamiana czasu na int:
+			for(int i=0; i<czas.length(); i++) {
+				if(czas.charAt(czas.length()-1-i) == '1') {
+					czasInt += (int)Math.pow(2, i);
+				}
+			}
+			
+			
+			// zamiana temperatury na int:
+			for(int i=0; i<temperatura.length()-1; i++) {
+				if(temperatura.charAt(temperatura.length()-1-i) == '1') {
+					temperaturaInt += (int)Math.pow(2, i);
+				}
+			}
+			if(temperatura.charAt(0) == '1'){
+				temperaturaInt += (int)Math.pow(2, temperatura.length()-1);
+			} else if(temperatura.charAt(0) == '-'){
+				temperaturaInt *= -1;
+			}
+			
+			
+			
+			Pomiar pomiar = new Pomiar();
+			pomiar.temperatura = temperaturaInt;
+			pomiar.czas = czasInt;
+			
+			pomiary1.add(pomiar);
+		}
+		
+		while(plik_dane2.hasNext()) {
+			String czas = plik_dane2.next();
+			String temperatura = plik_dane2.next();
+			
+			Integer czasInt = 0;
+			Integer temperaturaInt = 0;
+			// zamiana czasu na int:
+			for(int i=0; i<czas.length(); i++) {
+				czasInt += (int)Math.pow(4, i) * Character.getNumericValue(czas.charAt(czas.length()-1-i));
+			}
+			// zamiana temperatury na int:
+			for(int i=0; i<temperatura.length()-1; i++) {
+				temperaturaInt += (int)Math.pow(4, i) * Character.getNumericValue(temperatura.length()-1-i);
+				if((int)Math.pow(4, i) * Character.getNumericValue(temperatura.length()-1-i) < 0) {
+					System.out.print("Tu jest b³¹d");
+				}
+			}
+			if(temperatura.charAt(0) != '-'){
+				//System.out.print("Nie ma minusa; ");
+				temperaturaInt += (int)Math.pow(4, temperatura.length()-1) * Character.getNumericValue(temperatura.charAt(0));
+				if((int)Math.pow(4, temperatura.length()-1) * Character.getNumericValue(temperatura.charAt(0)) < 0) {
+					System.out.print("Tu jest b³¹d");
+				}
+			} else {
+				//System.out.print("Jest minus; ");
+				//System.out.print(temperaturaInt + ";;;");
+				//temperaturaInt *= -1;
+				//System.out.print(temperaturaInt);
+			}
+			
+			//System.out.println("temperaturaInt: " + temperaturaInt  + ", temperaturaStr: " + temperatura  + ", char at 0: " + temperatura.charAt(0));
+			
+			Pomiar pomiar = new Pomiar();
+			pomiar.temperatura = temperaturaInt;
+			pomiar.czas = czasInt;
+			
+			pomiary2.add(pomiar);
+		}
+		
+		while(plik_dane3.hasNext()) {
+			String czas = plik_dane3.next();
+			String temperatura = plik_dane3.next();
+			
+			Integer czasInt = 0;
+			Integer temperaturaInt = 0;
+			// zamiana czasu na int:
+			for(int i=0; i<czas.length(); i++) {
+				czasInt += (int)Math.pow(8, i) * Character.getNumericValue(czas.charAt(czas.length()-1-i));
+			}
+			// zamiana temperatury na int:
+			for(int i=0; i<temperatura.length()-1; i++) {
+				temperaturaInt += (int)Math.pow(8, i) * Character.getNumericValue(temperatura.length()-1-i);
+			}
+			if(temperatura.charAt(0) != '-'){
+				temperaturaInt += (int)Math.pow(4, temperatura.length()-1) * Character.getNumericValue(temperatura.charAt(0));
+			} else {
+				temperaturaInt *= -1;
+			}
+			
+			Pomiar pomiar = new Pomiar();
+			pomiar.temperatura = temperaturaInt;
+			pomiar.czas = czasInt;
+			
+			pomiary3.add(pomiar);
+		}
+		
+		Runnable pp1 = () ->{
+			int min1 = 999;
+			int min2 = 999;
+			int min3= 999;
+			
+			for(Pomiar pomiar : pomiary1) {
+				//System.out.println(pomiar.temperatura );
+				if(pomiar.temperatura < min1) {
+					min1 = pomiar.temperatura;
+				}
+			}
+			for(Pomiar pomiar2 : pomiary2) {
+				if(pomiar2.temperatura < min2) {
+					min2 = pomiar2.temperatura;
+				}			
+			}
+			for(Pomiar pomiar3 : pomiary3) {
+				if(pomiar3.temperatura < min3) {
+					min3 = pomiar3.temperatura;
+				}
+			}
+			
+			//System.out.println(pomiary1);
+			//System.out.println(pomiary2);
+			//System.out.println(pomiary3);
+			
+			String min1Binarnie = Integer.toBinaryString(Math.abs(min1));
+			String min2Binarnie = Integer.toBinaryString(Math.abs(min2));
+			String min3Binarnie = Integer.toBinaryString(Math.abs(min3));
+			
+			if(min1<0) {
+				min1Binarnie = "-" + min1Binarnie;
+			}
+			if(min2<0) {
+				min2Binarnie = "-" + min2Binarnie;	
+			}
+			if(min3<0) {
+				min3Binarnie = "-" + min3Binarnie;
+			}
+			
+			printWriter.println("Zadanie 58.1:");
+			printWriter.println();
+			printWriter.println("Najni¿sza odnotowana temperatura w stacji 1: " + min1);
+			printWriter.println("Najni¿sza odnotowana temperatura w stacji 2: " + min2);
+			printWriter.println("Najni¿sza odnotowana temperatura w stacji 3: " + min3);
+			printWriter.flush();
+		};
+		Runnable pp2 = () ->{
+			int ileBlednychPomiarow = 0;
+			for(int i=0; i<pomiary1.size(); i++) {
+				if(pomiary1.get(i).czas != pomiary2.get(i).czas || pomiary1.get(i).czas != pomiary3.get(i).czas) {
+					ileBlednychPomiarow++;
+				}
+			}
+			printWriter.println();
+			printWriter.println("Zadanie 58.2:");
+			printWriter.println();
+			printWriter.println("Liczba b³êdnych pomiarów: " + ileBlednychPomiarow);
+			printWriter.flush();
+		};
+		Runnable pp3 = () ->{
+		};
+		Runnable pp4 = () ->{
+		};
+		
+		pp1.run();
+		pp2.run();
+		pp3.run();
+		pp4.run();
+	}
+	private static void zadanie59() throws Exception {
+		
+		Scanner plikLiczby = new Scanner(new File("Files/59/liczby.txt"));
+		
+		PrintWriter printWriter = new PrintWriter(new File("rozwiazania/59.txt"));
+		
+		ArrayList<Integer> liczby = new ArrayList<Integer>();
+		
+		while(plikLiczby.hasNext()) {
+			liczby.add(plikLiczby.nextInt());
+		}
+		
+		Runnable pp1 = () ->{
+			int ileLiczbSpelniajacychWarunki = 0;
+			
+			for(Integer liczba : liczby) {
+				int liczbaCp = liczba;
+				ArrayList<Integer> czynnikiPierwsze = new ArrayList<Integer>();
+				
+				for(int i=2; i<=Math.round(Math.sqrt(liczba)); i++) {
+					if(liczbaCp % i == 0) {
+						czynnikiPierwsze.add(i);
+						liczbaCp /= i;
+						i=1; // will be 2 after i++
+					}
+				}
+				if(liczbaCp>1) {
+					czynnikiPierwsze.add(liczbaCp);
+				}	
+				
+				HashSet<Integer> rozneCzynnikiPierwsze = new HashSet<Integer>(czynnikiPierwsze);
+				
+				boolean czyMaParzystyDzielnik = false;
+				for (Iterator<Integer> i = rozneCzynnikiPierwsze.iterator(); i.hasNext();) {
+				    Integer element = i.next();
+				    if (element % 2 == 0) {
+				        // liczba nie spelnia warunku
+				    	czyMaParzystyDzielnik = true;
+				    	break;
+				    }
+				}
+				
+				if(rozneCzynnikiPierwsze.size() == 3 && !czyMaParzystyDzielnik) {
+					ileLiczbSpelniajacychWarunki++;
+				}
+			}
+			printWriter.println("podpunkt 1:");
+			printWriter.println("Iloœæ liczb maj¹cych dok³adnie 3 ró¿ne nieparzyste czynniki pierwsze: " + ileLiczbSpelniajacychWarunki);
+			printWriter.flush();
+		};
+		Runnable pp2 = () -> {
+			int ilePalindromow = 0;
+			for(int i=0; i<liczby.size(); i++) {
+				int liczba = liczby.get(i);
+				int odwrocona = Integer.parseInt(new StringBuilder(liczby.get(i).toString()).reverse().toString());
+				String suma = String.valueOf(odwrocona + liczba);
+				if(suma.equals(new StringBuilder(suma).reverse().toString())) {
+					ilePalindromow++;
+				}
+			}
+			printWriter.println();
+			printWriter.println("podpunkt 2:");
+			printWriter.println("Iloœæ palindromów: " + ilePalindromow);
+			printWriter.flush();
+		};
+		Runnable pp3 = () ->{
+			HashMap<Integer, ArrayList<Integer>> moc_liczby = new HashMap<Integer, ArrayList<Integer>>();
+			
+			for(int i=1; i<=8; i++) {
+				moc_liczby.put(i, new ArrayList<Integer>());
+			}
+		
+			for(int i=0; i<liczby.size(); i++) {
+				String liczbaString = liczby.get(i).toString();
+				int iloczyn = Character.getNumericValue(liczbaString.charAt(0));
+				for(int j=1; j<liczbaString.length(); j++) {;
+					iloczyn *= Character.getNumericValue(liczbaString.charAt(j));
+				}
+				int moc = 1;
+				while(iloczyn > 9) {
+					String iloczynString = String.valueOf(iloczyn);
+					iloczyn = Character.getNumericValue(iloczynString.charAt(0));
+					for(int j=1; j<iloczynString.length(); j++) {
+						iloczyn *= Character.getNumericValue(iloczynString.charAt(j));
+					}
+					moc++;
+				}
+				moc_liczby.get(moc).add(liczby.get(i));
+			}
+			
+			printWriter.println();
+			printWriter.println("podpunkt 3:");
+			for(int i=1; i<=8; i++) {
+				printWriter.println("Iloœæ liczb o mocy: " + i +  " wynosi " + moc_liczby.get(i).size());
+			}
+			printWriter.println("Minimalna liczba o mocy 1: " + Collections.min(moc_liczby.get(1)));
+			printWriter.print("Maksymalna liczba o mocy 1: " + Collections.max(moc_liczby.get(1)));
+			printWriter.flush();
+			
+		};
+			
+		pp1.run();
+		pp2.run();
+		pp3.run();
+	}
+	private static void zadanie60() throws Exception{
+		Scanner plikLiczby = new Scanner(new File("Files/60/liczby.txt"));
+		PrintWriter printWriter = new PrintWriter(new File("rozwiazania/60.txt"));
+		
+		ArrayList<Integer> liczby = new ArrayList<Integer>();
+		
+		while(plikLiczby.hasNext()) {
+			liczby.add(plikLiczby.nextInt());
+		}
+		
+		Runnable pp1 = () ->{
+			int ileMniejszychOd1000 = 0;
+			int przedostatnia = 0;
+			int ostatnia = 0;
+			for(int i=0; i<liczby.size(); i++) {
+				if(liczby.get(i) < 1000) {
+					ileMniejszychOd1000++;
+					przedostatnia = ostatnia;
+					ostatnia = liczby.get(i);
+				}
+			}
+			printWriter.println("Podpunkt 1: ");
+			printWriter.println("Iloœæ liczb mniejszych od 1000: " + ileMniejszychOd1000);
+			printWriter.println("Dwie ostatnie liczby: " + przedostatnia + ", " + ostatnia);
+			printWriter.flush();
+			
+		};
+		Runnable pp2 = () ->{
+			printWriter.println();
+			printWriter.println("Podpunkt 2: ");
+			for(int i=0; i<liczby.size(); i++) {
+				int liczba = liczby.get(i);
+				ArrayList<Integer> dzielniki = new ArrayList<Integer>();
+				for(int j=1; j<=liczba; j++) {
+					if(liczba % j == 0) {
+						dzielniki.add(j);
+					}
+				}
+				
+				if(dzielniki.size() == 18) {
+					printWriter.println(liczba);
+					printWriter.println(dzielniki);
+				}
+			}
+			printWriter.flush();
+		};
+		Runnable pp3 = () ->{
+			printWriter.println();
+			Collections.sort(liczby);
+			Collections.reverse(liczby);
+			
+			for(int i=0; i<liczby.size(); i++) {
+				int liczba = liczby.get(i);
+				ArrayList<Integer> dzielnikiPoza1 = new ArrayList<Integer>();
+				for(int j=2; j<=liczba; j++) {
+					if(liczba % j == 0) {
+						dzielnikiPoza1.add(j);
+					}
+				}
+				
+				boolean czyJestWzgledniePierwsza = true;
+				for(int j=i+1; j<liczby.size()-i-1; j++) {
+					czyJestWzgledniePierwsza = true;
+					for(int k=0; k<dzielnikiPoza1.size(); k++) {
+						if(liczby.get(j) % dzielnikiPoza1.get(k) == 0) {
+							// liczba nie jest wzglednie pierwsza;
+							czyJestWzgledniePierwsza = false;
+							j = 100000;
+							break;
+						}
+					}	
+				}
+				if(czyJestWzgledniePierwsza) {
+					printWriter.println();
+					printWriter.println("Podpunkt 3: ");
+					printWriter.print("Najwiêksza wzglêdnie pierwsza liczba to: " + liczba);
+					printWriter.flush();
+					break;
+				}	
+			}
+		};
+		
+		pp1.run();
+		pp2.run();
+		pp3.run();
+	}
+	private static void zadanie61() throws Exception{
+		PrintWriter printWriter = new PrintWriter(new File("rozwiazania/61.txt"));
+		
+		Scanner plikCiagi = new Scanner(new File("Files/61/ciagi.txt"));
+		Scanner plikBledne = new Scanner(new File("Files/61/bledne.txt"));
+		
+		ArrayList<ArrayList<Integer>> ciagi = new ArrayList<ArrayList<Integer>>();
+		
+		while(plikCiagi.hasNext()) {
+			int dlugoscCiagu = plikCiagi.nextInt();
+			ArrayList<Integer> aktualnyCiag = new ArrayList<Integer>();
+			
+			for(int i=0; i<dlugoscCiagu; i++) {
+				aktualnyCiag.add(plikCiagi.nextInt());
+			}
+			ciagi.add(aktualnyCiag);
+		}
+		Runnable pp1 = () ->{
+			ArrayList<ArrayList<Integer>> ciagiArytmetyczne = new ArrayList<ArrayList<Integer>>();
+			
+			for(int i=0; i<ciagi.size(); i++) {
+				ArrayList<Integer> ciag = ciagi.get(i);
+				int roznica = ciag.get(1) - ciag.get(0);
+				boolean czyArytmetyczny = true;
+				for(int j=2; j<ciag.size(); j++) {
+					if(ciag.get(j) - ciag.get(j-1) != roznica) {
+						czyArytmetyczny = false;
+						break;
+					}
+				}
+				if(czyArytmetyczny) {
+					ciagiArytmetyczne.add(ciag);
+				}
+			}
+			
+			int maxRoznica = -1;
+			
+			for(int i=0; i<ciagiArytmetyczne.size(); i++) {
+				int roznica = ciagiArytmetyczne.get(i).get(1) - ciagiArytmetyczne.get(i).get(0);
+				if(roznica > maxRoznica) {
+					maxRoznica = roznica;
+				}
+			}
+			
+			printWriter.println("Podpunkt 1:");
+			printWriter.println("Iloœæ ci¹gów arytmetycznych: " + ciagiArytmetyczne.size());
+			printWriter.println("Maksymalna ró¿nica ci¹gu arytmetycznego: " + maxRoznica);
+			printWriter.flush();
+		};
+		Runnable pp2 = () ->{
+			printWriter.println();
+			printWriter.println("Podpunkt 2:");
+			for(int i=0; i<ciagi.size(); i++) {
+			}
+			printWriter.flush();
+		};
+		Runnable pp3 = () ->{
+			
+		};
+		
+		pp1.run();
+		pp2.run();
+		pp3.run();
+	}
+	
+	
+	public static void main(String[] args) {
+		try {
+//			zadanie58();
+//			zadanie59();
+//			zadanie60();
+			zadanie61();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/zrobioneZadania.java
@@ -0,0 +1,299 @@
+public enum zrobioneZadania {
+    _58_1(false),
+	_58_2(false),
+	_58_3(false),
+	_58_4(false),
+	
+	_59_1(true),
+	_59_2(true),
+	_59_3(true),
+	
+	_60_1(true),
+	_60_2(true),
+	_60_3(true),
+	
+	_61_1(true),
+	_61_2(false),
+	_61_3(false),
+	
+	_62_1(false),
+	_62_2(false),
+	_62_3(false),
+	_62_4(false),
+	
+	_63_1(false),
+	_63_2(false),
+	_63_3(false),
+	
+	_64_1(false),
+	_64_2(false),
+	_64_3(false),
+	_64_4(false),
+	
+	_65_1(false),
+	_65_2(false),
+	_65_3(false),
+	_65_4(false),
+	
+	_66_1(false),
+	_66_2(false),
+	_66_3(false),
+	_66_4(false),
+	
+	_67_1(false),
+	_67_2(false),
+	_67_3(false),
+	_67_4(false),
+	
+	_68_1(false),
+	_68_2(false),
+	_68_3(false),
+	
+	_69_1(false),
+	_69_2(false),
+	_69_3(false),
+	_69_4(false),
+	
+	_70_1(false),
+	_70_2(false),
+	_70_3(false),
+	
+	_71_1(false),
+	_71_2(false),
+	_71_3(false),
+	
+	_72_1(false),
+	_72_2(false),
+	_72_3(false),
+	
+	_73_1(false),
+	_73_2(false),
+	_73_3(false),
+	
+	_74_1(false),
+	_74_2(false),
+	_74_3(false),
+	_74_4(false),
+	
+	_75_1(false),
+	_75_2(false),
+	_75_3(false),
+	
+	_76_1(false),
+	_76_2(false),
+	_76_3(false),
+	
+	_77_1(false),
+	_77_2(false),
+	_77_3(false),
+	
+	_78_1(false),
+	_78_2(false),
+	_78_3(false),
+	
+	_79_1(false),
+	_79_2(false),
+	_79_3(false),
+	_79_4(false),
+	
+	_80_1(false),
+	_80_2(false),
+	_80_3(false),
+	
+	_81_1(false),
+	_81_2(false),
+	_81_3(false),
+	_81_4(false),
+	_81_5(false),
+	
+	_82_1(false),
+	_82_2(false),
+	_82_3(false),
+	_82_4(false),
+	_82_5(false),
+	
+	_83_1(false),
+	_83_2(false),
+	_83_3(false),
+	_83_4(false),
+	
+	_84_1(false),
+	_84_2(false),
+	_84_3(false),
+	_84_4(false),
+	
+	_85_1(false),
+	_85_2(false),
+	_85_3(false),
+	_85_4(false),
+	_85_5(false),
+	_85_6(false),
+	
+	_86_1(false),
+	_86_2(false),
+	_86_3(false),
+	_86_4(false),
+	_86_5(false),
+	
+	_87_1(false),
+	_87_2(false),
+	_87_3(false),
+	_87_4(false),
+	_87_5(false),
+	_87_6(false),
+	
+	_88_1(false),
+	_88_2(false),
+	_88_3(false),
+	_88_4(false),
+	_88_5(false),
+	
+	_89_1(false),
+	_89_2(false),
+	_89_3(false),
+	_89_4(false),
+	_89_5(false),
+	
+	_90_1(false),
+	_90_2(false),
+	_90_3(false),
+	_90_4(false),
+	_90_5(false),
+	
+	_91_1(false),
+	_91_2(false),
+	_91_3(false),
+	_91_4(false),
+	_91_5(false),
+	
+	_92_1(false),
+	_92_2(false),
+	_92_3(false),
+	_92_4(false),
+	_92_5(false),
+	
+	_93_1(false),
+	_93_2(false),
+	_93_3(false),
+	_93_4(false),
+	_93_5(false),
+	
+	_94_1(false),
+	_94_2(false),
+	_94_3(false),
+	_94_4(false),
+	_94_5(false),
+	_94_6(false),
+	
+	_95_1(false),
+	_95_2(false),
+	_95_3(false),
+	_95_4(false),
+	_95_5(false),
+	
+	_96_1(false),
+	_96_2(false),
+	_96_3(false),
+	_96_4(false),
+	_96_5(false),
+	_96_6(false),
+	
+	_97_1(false),
+	_97_2(false),
+	_97_3(false),
+	_97_4(false),
+	_97_5(false),
+	
+	_98_1(false),
+	_98_2(false),
+	_98_3(false),
+	_98_4(false),
+	_98_5(false),
+	
+	_99_1(false),
+	_99_2(false),
+	_99_3(false),
+	_99_4(false),
+	_99_5(false),
+	_99_6(false),
+	
+	_100_1(false),
+	_100_2(false),
+	_100_3(false),
+	_100_4(false),
+	_100_5(false),
+	
+	_101_1(false),
+	_101_2(false),
+	_101_3(false),
+	_101_4(false),
+	_101_5(false),
+	
+	_102_1(false),
+	_102_2(false),
+	_102_3(false),
+	_102_4(false),
+	_102_5(false),
+	
+	_103_1(false),
+	_103_2(false),
+	_103_3(false),
+	_103_4(false),
+	_103_5(false),
+	
+	_104_1(false),
+	_104_2(false),
+	_104_3(false),
+	_104_4(false),
+	_104_5(false),
+	
+	_105_1(false),
+	_105_2(false),
+	_105_3(false),
+	_105_4(false),
+	_105_5(false),
+	
+	_106_1(false),
+	_106_2(false),
+	_106_3(false),
+	_106_4(false),
+	_106_5(false),
+	
+	_107_1(false),
+	_107_2(false),
+	_107_3(false),
+	_107_4(false),
+	_107_5(false),
+	
+	_108_1(false),
+	_108_2(false),
+	_108_3(false),
+	_108_4(false),
+	_108_5(false),
+	
+	_109_1(false),
+	_109_2(false),
+	_109_3(false),
+	_109_4(false),
+	_109_5(false),
+	
+	_110_1(false),
+	_110_2(false),
+	_110_3(false),
+	_110_4(false),
+	_110_5(false),
+	
+	_111_1(false),
+	_111_2(false),
+	_111_3(false),
+	_111_4(false),
+	_111_5(false),
+	
+	_112_1(false),
+	_112_2(false),
+	_112_3(false),
+	_112_4(false),
+	_112_5(false);
+	
+	zrobioneZadania(boolean czyZrobione) {}
+}
